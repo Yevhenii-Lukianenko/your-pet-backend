4e4721b3f07ebecb95460c98e0e4410b9b9cc6b3
Yevhenii-Lukianenko <zhenyalukyanenko17@gmail.com>
2023-08-25 17:15:36 +0300

    fixed add pet route

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,70 +1,48 @@
-const {Pets} = require("../models/pets");
-const {
-    uploadImageToCloudinary
-  } = require("../utils");
+const { Pets } = require("../models/pets");
+const { uploadImageToCloudinary } = require("../utils");
 
-const {ctrlWrapper, HttpError} = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
-const getAll = async(req, res) => {
-    const {_id: owner} = req.user;
-    console.log(req.params);
-    const {page = 1, limit = 5} = req.query;
-    const skip = (page -1) *limit;
-    const result = await Pets.find({owner}, "-createAt -updateAt", {skip, limit});
-    res.json(result)
-}
-
-const add = async(req, res) => {
-    const {_id: owner } = req.user;
-
-    const { path: tempUpload } = req.file;
-    const image = await uploadImageToCloudinary(tempUpload);
-
-    const result = await Pets.create({
-        ...req.body, 
-        avatarPet: image.url,
-        owner
-    }); 
-
-    res.status(201).json(result);
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  console.log(req.params);
+  const { page = 1, limit = 5 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Pets.find({ owner }, "-createAt -updateAt", {
+    skip,
+    limit,
+  });
+  res.json(result);
 };
 
+const add = async (req, res) => {
+  const { _id: owner } = req.user;
 
-// const add = async(req, res) => {
-//     const {_id: owner } = req.user;
+  const { path: tempUpload } = req.file;
+  const image = await uploadImageToCloudinary(tempUpload);
 
-//     const result = await Pets.create({
-//         ...req.body, 
-//         owner
-//     }); 
+  const result = await Pets.create({
+    ...req.body,
+    avatarPet: image.url,
+    owner,
+  });
 
-//     res.status(201).json(result);
-// };
+  res.status(201).json(result);
+};
 
-// const addImagePet = async(req, res) => {
-//   const {id} = req.params;
-//   const { path: tempUpload } = req.file;
-//   const image = await uploadImageToCloudinary(tempUpload);
-
-//   const result = await Pets.findByIdAndUpdate(id, { avatarPet: image.url})
-//     res.status(201).json(result);
-// }
- 
-const remove = async(req, res) => {
-    const {id} = req.params;
-    const result = await Pets.findByIdAndDelete({_id: id});
-    if(!result){
-        throw HttpError(404, "Not Found");
-    }
-    res.json({
-        message: "Delete success"
-    });
+const remove = async (req, res) => {
+  const { id } = req.params;
+  const result = await Pets.findByIdAndDelete({ _id: id });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json({
+    message: "Delete success",
+  });
 };
 
 module.exports = {
-    add: ctrlWrapper(add),
-    getAll: ctrlWrapper(getAll),
-    // addImagePet: ctrlWrapper(addImagePet),
-    remove: ctrlWrapper(remove),
-  
-}
+  add: ctrlWrapper(add),
+  getAll: ctrlWrapper(getAll),
+  remove: ctrlWrapper(remove),
+};
--- middlewares/imageProcessing.js
+++ middlewares/imageProcessing.js
@@ -1,11 +1,16 @@
 const jimp = require("jimp");
+const { HttpError } = require("../helpers");
 
 const imageProcessing = async (req, res, next) => {
   const { file } = req;
-  const image = await jimp.read(file.path);
-  image.quality(80);
-  image.write(file.path);
-  next();
+  if (!file) {
+    next(HttpError(400, "No added image"));
+  } else {
+    const image = await jimp.read(file.path);
+    image.quality(80);
+    image.write(file.path);
+    next();
+  }
 };
 
 module.exports = imageProcessing;
